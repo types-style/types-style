32234d87da45811b5a4d99639ee1b9c0be23eda0
Balaj Marius <balajmarius93@gmail.com>
2020-09-29 14:46:50 +0300

    Update Container.js

--- src/atoms/Container.js
+++ src/atoms/Container.js
@@ -3,9 +3,10 @@ import styled from "styled-components";
 import { SPACING, SIZES } from "../utilities/const";
 
 const Container = styled.div`
-  width: ${SIZES.container};
+  min-height: 100vh;
   margin-left: auto;
   margin-right: auto;
+  width: ${SIZES.container};
   margin-top: ${SPACING.sm};
   margin-bottom: ${SPACING.sm};
 `;
