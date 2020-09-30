fc951f46a83742855ff16e5305eb3235d76aae41
Balaj Marius <balajmarius93@gmail.com>
2020-09-30 19:01:04 +0300

    added mobile version

--- src/atoms/Column.js
+++ src/atoms/Column.js
@@ -1,12 +1,34 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
-import { SPACING } from "../utilities/const";
+import { SPACING, MEDIA_QUERIES } from "../utilities/const";
 
 const Column = styled.div`
   width: 50%;
   position: relative;
-  margin-left: ${SPACING.lg};
-  margin-right: ${SPACING.lg};
+  padding-left: ${SPACING.lg};
+  padding-right: ${SPACING.lg};
+
+  @media ${MEDIA_QUERIES.large} {
+    padding-left: ${SPACING.md};
+    padding-right: ${SPACING.md};
+    width: ${({ last }) => last ? "35%" : "65%"};
+  }
+
+  @media ${MEDIA_QUERIES.small} {
+    width: 100%;
+    padding-left: ${SPACING.sm};
+    padding-right: ${SPACING.sm};
+    margin-top: ${({ last }) => last && SPACING.lg};
+  }
 `;
+
+Column.propTypes = {
+  last: PropTypes.bool
+};
+
+Column.defaultProps = {
+  last: false
+};
 
 export default Column;
--- src/atoms/Typography.js
+++ src/atoms/Typography.js
@@ -1,11 +1,15 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-import { COLORS, FONT_SIZE } from "../utilities/const";
+import { COLORS, FONT_SIZE, MEDIA_QUERIES } from "../utilities/const";
 
 const Typography = styled.span`
   color: ${COLORS.darkGray};
   font-size: ${({ variant }) => FONT_SIZE[variant]};
+
+  @media ${MEDIA_QUERIES.medium} {
+    font-size: ${({ variant }) => variant === "title" && FONT_SIZE.subtitle};
+  }
 `;
 
 Typography.propTypes = {
